9496f919fb92bbe1344453a2a0cb7cf74362cea6
Yennic0302 <yender.yr45@gmail.com>
2022-08-31 21:22:13 -0400

    update memory responsive

--- src/components/NavVar.jsx
+++ src/components/NavVar.jsx
@@ -1,25 +1,30 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { BsFillCalendar3RangeFill } from "react-icons/bs";
-import './NavVar.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./NavVar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function MemoryNavBar({setSeeHistory,seeBtnHistory}) {
+function MemoryNavBar({ setSeeHistory, seeBtnHistory }) {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
-						<h1 style={{fontSize: "2rem"}}>Memory</h1>
-					</Navbar.Brand>
-          <Nav className="me-auto">
-          </Nav>
+            <h1 className="title-nav" style={{ fontSize: "2rem" }}>
+              Memory
+            </h1>
+          </Navbar.Brand>
+          <Nav className="me-auto"></Nav>
         </Container>
-        <button 
-        className='btn-history'
-        style={seeBtnHistory?{display: 'block'}:{display: "none"}} 
-        onClick={()=>setSeeHistory(true)}> <BsFillCalendar3RangeFill/> </button>
+        <button
+          className="btn-history"
+          style={seeBtnHistory ? { display: "block" } : { display: "none" }}
+          onClick={() => setSeeHistory(true)}
+        >
+          {" "}
+          <BsFillCalendar3RangeFill />{" "}
+        </button>
       </Navbar>
     </>
   );
